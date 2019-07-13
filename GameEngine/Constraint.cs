6edc737c9a1cbeef0cc2b637f116dd26c83e05e1
Gal Weiss <gaweiss@microsoft.com>
2019-07-14 01:10:50 +0300

    Add project files.

---
+++ GameEngine/Constraint.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchingGame.GameEngine
+{
+    abstract class Constraint
+    {
+        abstract public bool checkMatch(Participant participant);
+    }
+
+    class GenderBlockConstraint : Constraint
+    {
+        private readonly Gender gender;
+
+        GenderBlockConstraint(Gender gender)
+        {
+            this.gender = gender;
+        }
+
+        override public bool checkMatch(Participant participant)
+        {
+            return !(gender == participant.gender);
+        }
+    }
+
+    class ParticipantBlockConstraint : Constraint
+    {
+        private readonly Participant participant;
+
+        ParticipantBlockConstraint(Participant participant)
+        {
+            this.participant = participant;
+        }
+
+        public override bool checkMatch(Participant participant)
+        {
+            return !(this.participant == participant);
+        }
+    }
+}
---
+++ GameEngine/Participant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchingGame.GameEngine
+{
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    class Participant
+    {
+        Participant (String name, Gender gender)
+        {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        public String name {get; }
+        public Gender gender {get; }
+
+        /* Overrides */
+
+        private static bool isEqual(Participant x, Participant y)
+        {
+            return (x.name == y.name && x.gender == y.gender);
+        }
+
+        public static bool operator ==(Participant x, Participant y)
+        {
+            return isEqual(x, y);
+        }
+
+        public static bool operator !=(Participant x, Participant y)
+        {
+            return !isEqual(x, y);
+        }
+    }
+}
